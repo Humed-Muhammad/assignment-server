df46e34a8eb5c41cb65ab1255552b48895cdc662
Humed-Muhammad <abc45171@gmail.com>
2023-02-12 17:41:40 +0300

    🔧 Adding dev test network

--- truffle-config.js
+++ truffle-config.js
@@ -68,11 +68,11 @@ contracts_build_directory: "../client/src/utils/abi",
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 7545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "1337",       // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -86,14 +86,14 @@ contracts_build_directory: "../client/src/utils/abi",
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    goerli: {
-      networkCheckTimeout: 10000, 
-      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
-      network_id: 5,       // Goerli's id
-      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
+    // goerli: {
+    //   networkCheckTimeout: 10000, 
+    //   provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
+    //   network_id: 5,       // Goerli's id
+    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
     //
     // Useful for private networks
     // private: {
